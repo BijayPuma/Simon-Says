ad2d313fcad9f890494b14a1361240ba105bba44
Bijay Rai <raibjraibj@yahoo.com>
2018-10-23 14:16:43 -0400

    button tracker and two buttons hide show

--- simon.js
+++ simon.js
@@ -2,8 +2,7 @@ let redButton = document.querySelector(".RedButton");
 let blueButton = document.querySelector(".BlueButton");
 let greenButton = document.querySelector(".GreenButton");
 let yellowButton = document.querySelector(".YellowButton");
-redButton.value = 1;
-blueButton.value = 2;
+
 let startButton = document.querySelector(".StartButton");
 let yourTurn = document.querySelector(".YourTurn");
 let score = document.querySelector(".Score");
@@ -11,30 +10,52 @@ let resetButton = document.querySelector(".Reset");
 
 let counter = 0;
 
-resetButton.addEventListener("click", function() {
-  counter = 0;
-  yourTurn.value = "Start Over, click start";
-  score.value = "Score: " + 0;
-});
+function whichButton() {
+  console.log(event.srcElement.className);
+}
+// startButton.addEventListener("click", function() {
+//   hideRedButton();
+//   showRedButton();
+
+//   for (var i = 900; i < 2300; i += 900) {
+//     setTimeout("hideRedButton()", i);
+//     setTimeout("showRedButton()", i + 450);
+//   }
+
+//   yourTurn.value = "Your Turn";
+//   counter++;
+// });
+
+// redButton.addEventListener("click", function() {
+//   score.value = "Score: " + counter;
+//   yourTurn.value = "Level 1 passed";
+// });
 
 startButton.addEventListener("click", function() {
   hideRedButton();
   showRedButton();
-  for (var i = 900; i < 2300; i += 900) {
+  hideBlueButton();
+  showBlueButton();
+
+  for (var i = 900; i < 1500; i += 900) {
     setTimeout("hideRedButton()", i);
     setTimeout("showRedButton()", i + 450);
+  }
+
+  for (var i = 1900; i < 3000; i += 1300) {
+    setTimeout("hideBlueButton()", i);
+    setTimeout("showBlueButton()", i + 800);
   }
 
   yourTurn.value = "Your Turn";
   counter++;
 });
 
-redButton.addEventListener("click", function() {
-  score.value = "Score: " + counter;
-  yourTurn.value = "Level 1 passed";
+resetButton.addEventListener("click", function() {
+  counter = 0;
+  yourTurn.value = "Start Over, click start";
+  score.value = "Score: " + 0;
 });
-
-blueButton.addEventListener("click", function() {});
 
 function hideRedButton() {
   redButton.style.visibility = "hidden";
